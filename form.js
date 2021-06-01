984c0cc0d42c8e4d32df3ae6ddcefad82cc2287d
Manikanta <duppanapudimanikanta43>
2021-06-01 10:07:43 -0700

    Form Assessment

---
+++ form.js
@@ -0,0 +1,47 @@
+const defaultForm = () => {
+  document.getElementById('radioOne').checked = true;
+	return {
+		name: { value: '', touched: false, filled: false },
+		email: { value: '', touched: false, filled: false },
+		phoneNumber: { value: '', touched: false, filled: false },
+		todaysDate: { value: '', touched: false, filled: false },
+		overDueDate: { value: '', touched: false, filled: false },
+		dateOfEvent: { value: '', touched: false, filled: false },
+		timeOfEvent: { value: '', touched: false, filled: false },
+		eventLocation: { value: '', touched: false, filled: false },
+		typeOfEvent: { value: '', touched: false, filled: false },
+		noOfGuests: { value: '', touched: false, filled: false },
+		pickUpTime: { value: '', touched: false, filled: false },
+		method: { value: 'pickUp', touched: false, filled: false },
+	};
+};
+let formData = defaultForm();
+const fieldHandler = (e) => {
+	const { name, value } = e.target;
+	formData[name] = { value, touched: true, filled: value.length !== 0 && true };
+};
+
+const formValidation = (formInputs) => {
+	let emptyFields = [];
+	for (const key in formInputs) {
+		if (!formInputs[key].value) {
+			emptyFields.push(key);
+		}
+	}
+	return emptyFields;
+};
+
+const formHandler = (e) => {
+	e.preventDefault();
+  alert('Form Submitted');
+	const forminfo = formValidation(formData);
+	console.log(
+		'form fields: ', formData,
+		{'counts of empty fields':
+		forminfo.length},
+		{'empty fields':
+		forminfo.join()}
+	);
+	e.target.reset();
+	formData = defaultForm();
+};
